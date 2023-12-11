420e24ed81c83cda983ae1b2d8a75a5b625ef892
boldybread <bradarchbold@gmail.com>
2023-12-12 07:56:29 +1100

    first commit

---
+++ main.py
@@ -0,0 +1,4 @@
+print("Welcome to Malice in Chains\n")
+print("Good luck on your adventure\n")
+print("You wake up in a strange place\n")
+print("The room is unfamiliar, there are chains around your wrists")
