b7276993ba14723d99410fd8984c4b1d6cb6f4a7
boldybread <bradarchbold@gmail.com>
2023-12-17 11:25:27 +1100

    fixed run.sh file, now works properly

--- run.sh
+++ run.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
 python3 -m venv .venv
-cd .venv
-source /bin/activate
+source .venv/bin/activate
 
 
 # check if python installed
-# [[ "$(python3 -V)" =~ "Python 3" ]] && echo "Python 3 is installed"
+[[ "$(python3 -V)" =~ "Python 3" ]] && echo "Python 3 is installed"
 
 
 
-pip3 install colored
-pip3 install fontstyle
+python3 -m pip install colored
+python3 -m pip install fontstyle
 
 
-#  sudo apt install pulseaudio -y
-# python3 main.py
+python3 main.py
