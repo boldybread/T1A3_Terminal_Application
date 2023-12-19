10b13892d7db6244f93e1bb6727705a7f38a71a3
boldybread <bradarchbold@gmail.com>
2023-12-19 19:22:31 +1100

    added setup.sh and fixed randomiser in random_encounter

--- run.sh
+++ run.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 
-python3 -m venv .venv
-source .venv/bin/activate
+./setup.sh
 
-
-# check if python installed
-[[ "$(python3 -V)" =~ "Python 3" ]] && echo "Python 3 is installed"
-
-
-
-python3 -m pip install colored
-python3 -m pip install fontstyle
-python3 -m pip install computational-stopwatch
-python3 -m pip install names_generator
-
+# Run the program
 python3 main.py
---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# # Check for venv and create if needed
+python3 -m venv .venv && echo "virtual environment is created"
+
+
+# activate venv
+source .venv/bin/activate && echo "virtual environment is activated"
+
+
+# check if python installed
+[[ "$(python3 -V)" =~ "Python 3" ]] && echo "Python 3 is already installed"
+
+
+# Install required packages
+# python3 -m pip install colored
+# python3 -m pip install fontstyle
+# python3 -m pip install computational-stopwatch
+# python3 -m pip install names
+
+python3 -m pip install -r requirements.txt
