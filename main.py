2eb966b8b59d77e72c2f841c2cda5ac00927a676
boldybread <bradarchbold@gmail.com>
2023-12-22 13:10:41 +1100

    changed wait to normal

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from computational_stopwatch import Stopwatch
 
 sw = Stopwatch() # Starts the timer which times completion time
 
-wait_duration = 0 # This is the variable used for most of the pauses between output lines of text. This enabled me to change to 0 for testing, or 4 for the proper game
+wait_duration = 4 # This is the variable used for most of the pauses between output lines of text. This enabled me to change to 0 for testing, or 4 for the proper game
 
 from intro import prologue
 prologue()
